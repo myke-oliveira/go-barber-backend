1a59bd034815917d60325bc61014e93ad3bd972f
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-12-13 18:31:04 -0300

    ✨ NEW FEATURE: Add token on authenticate

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from "jsonwebtoken";
 import User from '../models/User';
 
 interface RequestDTO {
@@ -11,7 +12,7 @@ class AuthenticateUserService {
   /**
    * execute
    */
-  public async execute({ email, password }: RequestDTO): Promise<{user: User}> {
+  public async execute({ email, password }: RequestDTO): Promise<{user: User, token: string}> {
     const userRepository = getRepository(User);
 
     const user = await userRepository.findOne({
@@ -30,7 +31,12 @@ class AuthenticateUserService {
 
     delete user.password;
 
-    return { user };
+    const token = sign({}, 'f5f26ab66cab70300fd5e113b5fc7764', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
